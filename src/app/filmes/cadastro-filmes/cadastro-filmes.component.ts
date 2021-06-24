5ddbeaab3dfd6f65171b3a5f09bcbdaf8ba4e2a0
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-06-23 21:29:03 -0300

    fixing signup screen

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -12,13 +12,36 @@ export class CadastroFilmesComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  get f() {
+    return this.cadastro.controls;
+  }
+
+  ngOnInit(): void {
 
     this.cadastro = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['',[Validators.required]],
+      descricao: [''],
+      nota: [0,[Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', Validators.required]
     });
 
+  }
+
+  salvar(): void {
+    this.cadastro.markAllAsTouched();
+    if (this.cadastro.invalid){
+      return;
+    }
+
+    alert('SUCESSO!! \n\n' + JSON.stringify(this.cadastro.value, null, 4));
+
+  }
+
+  reiniciarForm(): void{
+    this.cadastro.reset();
   }
 
 }
